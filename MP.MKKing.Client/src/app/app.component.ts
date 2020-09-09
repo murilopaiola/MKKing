61b5749718ea8afd5509b7feb50e8757b5123f76
murilopaiola <53261562+murilopaiola@users.noreply.github.com>
2020-09-09 16:54:10 -0300

    Added HttpClient and changed Logo

--- MP.MKKing.Client/src/app/app.component.ts
+++ MP.MKKing.Client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mechanical Keyboards King';
+  products: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
+    }, err => {
+      console.log(err);
+    })
+  }
 }
