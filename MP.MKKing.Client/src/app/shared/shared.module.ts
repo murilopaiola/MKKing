ab7c100ff3ce583929c220aa66183f2d82fbbc30
murilopaiola <53261562+murilopaiola@users.noreply.github.com>
2020-09-10 10:33:16 -0300

    added pagination shared component

--- MP.MKKing.Client/src/app/shared/shared.module.ts
+++ MP.MKKing.Client/src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 
 // Shared modules
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
     // need forRoot() to load PaginationModule with its providers
     PaginationModule.forRoot()
   ],
-  exports: [PaginationModule]
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagerComponent
+  ]
 })
 export class SharedModule { }
--- MP.MKKing.Client/src/app/shop/shop.component.ts
+++ MP.MKKing.Client/src/app/shop/shop.component.ts
@@ -74,7 +74,7 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any): void{
-    this.shopParams.pageNumber = event.page;
+    this.shopParams.pageNumber = event;
     this.getProducts();
   }
 
