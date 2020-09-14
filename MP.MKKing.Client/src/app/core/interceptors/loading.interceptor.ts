f29b49f4a46dffa3053c85b26e2b6cc203c2f567
murilopaiola <53261562+murilopaiola@users.noreply.github.com>
2020-09-14 01:41:52 -0300

    added loading interceptor and home component

--- MP.MKKing.Client/src/app/core/interceptors/loading.interceptor.ts
+++ MP.MKKing.Client/src/app/core/interceptors/loading.interceptor.ts
@@ -17,6 +17,6 @@ export class LoadingInterceptor implements HttpInterceptor {
             finalize(() => {
                 this.busyService.idle();
             })
-        )
+        );
     }
 }
--- MP.MKKing.Client/src/app/shared/shared.module.ts
+++ MP.MKKing.Client/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 
@@ -11,12 +12,14 @@ import { PagerComponent } from './components/pager/pager.component';
   imports: [
     CommonModule,
     // need forRoot() to load PaginationModule with its providers
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
